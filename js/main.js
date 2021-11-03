ccb083ebd7e2525d5501f4c235e260694b7b2dcc
nerzul <88493588+n3rzul@users.noreply.github.com>
2021-11-03 23:12:01 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+    $('.slider__inner, .newsslider__inner').slick({
+        nextArrow:'<button type="button" class=" slick__btn slick-next"></button>',
+        prevArrow:'<button type="button" class="slick__btn slick-prev"></button>',
+        infinite:false,
+
+    });
+
+    $('select').styler();
+
+
+
+
+});
